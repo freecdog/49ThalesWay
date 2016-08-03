3a46ef0541d2c84e008a1a046bfb54db2277bef9
999fff <mm1000@list.ru>
2016-08-03 18:53:12 +0300

    Initial commit

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -4,7 +4,22 @@
 
 (function(window){
 
-    var core = {};
+    var core = (function () {
+        // https://github.com/podgorniy/javascript-toolbox/blob/master/singletone.js
+        var instance = {};
+
+        return function Construct_singletone () {
+            if (instance) {
+                return instance;
+            }
+            if (this && this.constructor === Construct_singletone) {
+                instance = this;
+            } else {
+                return new Construct_singletone();
+            }
+        };
+    }());
+    window.core = core;
 
     core.version = "0.0.1";
 
@@ -23,14 +38,15 @@
             }
         };
     }());
+    core.generation = generation;
+
     function generateId(){
         //var generation = new generation();
-        //generation.idCounter = generation.idCounter | 0;
+        generation.idCounter = generation.idCounter | 0;
         var ans = generation.idCounter;
         generation.idCounter++;
         return ans;
     }
-    core.generation = generation;
     core.generateId = generateId;
 
     function Person(){
@@ -48,6 +64,7 @@
         self.farms = [];
         function grabFarm(farm){
             self.farms.push(farm);
+            farm.changeOwner(self);
         }
 
         return this;
@@ -65,9 +82,53 @@
         options.resourceAmmount = 1;
         options.startTime = 0;
         options.finishTime = 0;
+        options.owner = null;
+
+        function changeOwner(newOwner){
+            options.owner = newOwner;
+        }
+        self.changeOwner = changeOwner;
     }
     core.Farm = Farm;
 
-    window.core = core;
+    function TimeLine(){
+        var self = this;
+
+        var options = {};
+        self.options = options;
+
+        options.active = true;
+
+        options.paused = false;
+        options.pauses = [];
+
+        options.startTime = Date.now();
+        options.round = 1000;       // ms
+
+        var currentTime = function(){return Date.now()};
+        self.currentTime = currentTime;
+
+        var timeFromStart = function(){
+            var ans = 0;
+            for (var i = 0; i < options.pauses.length; i++){
+                if (i%2 == 1) ans -= (options.pauses[i] - options.pauses[i-1]);
+            }
+            ans += currentTime() - options.startTime;
+            return ans;
+        };
+        self.timeFromStart = timeFromStart;
+
+        var roundFromStart = function(){return Math.floor(timeFromStart() / options.round)};
+        self.roundFromStart = roundFromStart;
+        self.currentRound = roundFromStart;
+
+        var pauseTimeLine = function(){
+            options.paused = !options.paused;
+            options.pauses.push(currentTime());
+        };
+        self.pauseTimeLine = pauseTimeLine;
+        self.pause = pauseTimeLine;
+    }
+    core.TimeLine = TimeLine;
 
 })(window);
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -10,6 +10,22 @@
         var p1 = new core.Farm();
         var p2 = new core.Farm();
         console.log(p1,p2);
+
+        var t = new core.TimeLine();
+        setTimeout(function(){
+            console.log(t.roundFromStart(), t.timeFromStart());
+        }, 1100);
+        setTimeout(function(){
+            t.pause();
+            console.log("paused");
+        }, 2200);
+        setTimeout(function(){
+            t.pause();
+            console.log("unpaused");
+        }, 3300);
+        setTimeout(function(){
+            console.log(t.roundFromStart(), t.timeFromStart());
+        }, 4400);
     } else {
         console.error("no window.core", window);
     }
